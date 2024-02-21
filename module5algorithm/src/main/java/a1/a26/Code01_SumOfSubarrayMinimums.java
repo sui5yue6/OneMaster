3440c6fef977b6e15f2a9e544ec4112d620ed72c
eric <942300755@qq.com>
2024-02-21 23:23:38 +0800

    commit algorithm

---
+++ module5algorithm/src/main/java/a1/a26/Code01_SumOfSubarrayMinimums.java
@@ -0,0 +1,21 @@
+package a1.a26;
+
+import java.util.Stack;
+
+/**
+ * @author Eirk
+ * @Description
+ * @Date 2024/2/20 0:32
+ */
+public class Code01_SumOfSubarrayMinimums {
+    public static void main(String[] args) {
+
+    }
+
+    public int sumSubarrayMins(int[] arr) {
+        // 这个题目相当的难。也终于知道为啥前面要把左边最小和右边最小写在一起了。
+        // 这个题目还是先做出来。单调栈的也太难了
+        // 核心还是从遍历每个数组的思维转换到
+        return 0;
+    }
+}
