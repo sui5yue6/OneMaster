4abc0f2b807f10c33ac9d25c776ba0887d838c5b
eric <942300755@qq.com>
2024-03-08 23:31:20 +0800

    commit kubernetes

---
+++ module5algorithm/src/main/java/a1/superEggDrop.java
@@ -0,0 +1,20 @@
+package a1;
+
+/**
+ * @author Eirk
+ * @Description
+ * @Date 2024/3/8 13:02
+ */
+public class superEggDrop {
+    public int superEggDrop(int k, int n) {
+
+        return f(k, n, 1, n);
+
+    }
+
+    public int f(int k, int n, int start, int end) {
+        return 0;
+    }
+
+
+}
