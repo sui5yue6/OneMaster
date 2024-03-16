a765d675f5033dc2058bc3b41f6893a88436cfa9
eric <942300755@qq.com>
2024-03-16 23:47:30 +0800

    commit algorithm

---
+++ module5algorithm/src/main/java/a1/a37/MedianSlidingWindow.java
@@ -0,0 +1,54 @@
+package a1.a37;
+
+/**
+ * @author Eirk
+ * @Description
+ * @Date 2024/3/15 1:33
+ */
+public class MedianSlidingWindow {
+    public double[] medianSlidingWindow(int[] nums, int k) {
+        for (int i = 0; i < nums.length - k; i++) {
+
+        }
+    }
+
+    public static class Node {
+        int key;
+        Node l;
+        Node r;
+        int size;
+
+        public Node(int key) {
+            this.key = key;
+            size = 1;
+        }
+    }
+
+    public static class TreeMap {
+        Node root;
+
+        private Node rightRotate(Node cur) {
+
+        }
+
+        private Node leftRotate(Node cur) {
+
+        }
+
+        private Node maintain(Node cur) {
+
+        }
+
+        private Node add(Node cur, int key) {
+
+        }
+
+        private Node delete(Node cur, int key) {
+
+        }
+
+        private Node getIndex(Node cur, int kth) {
+
+        }
+    }
+}
